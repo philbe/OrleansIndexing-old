bb3ea74678d5d3728882aa3a684f8b2ebf52f0dd
Mohammad Dashti <mdashti@gmail.com>
2016-06-09 11:46:07 -0700

    Added the interface for hash and range indexes.

---
+++ src/OrleansIndexing/IHashIndex.cs
@@ -0,0 +1,59 @@
+﻿using Orleans;
+using Orleans.Concurrency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrleansIndexing
+{
+    /// <summary>
+    /// Defines the interface for hash-indexes
+    /// </summary>
+    /// <typeparam name="K">the type of key for the hash index</typeparam>
+    /// <typeparam name="V">the type of grain interface that is
+    /// being indexed</typeparam>
+    interface IHashIndex<K,V> : IIndex where V : IGrain
+    {
+        /// <summary>
+        /// hash-indexes can be either unique or non-unique.
+        /// If the user defines a hash-index as a unique hash-index,
+        /// then we want to guarantee that uniqueness. This method
+        /// determines whether this hash-index is a unique hash-index.
+        /// </summary>
+        /// <returns>true, if there should be a single grain
+        /// associated with each key, otherwise false</returns>
+        Task<bool> IsUnique();
+
+        /// <summary>
+        /// Each hash-index needs a hash function, and a user can specify
+        /// the hash function via a call to this method.
+        /// 
+        /// This method should be used internally by the index grain and
+        /// should not be invoked from other grains.
+        /// </summary>
+        /// <param name="hashLambda">hash function that should be used
+        /// for this hash-index</param>
+        void SetHashLambda(Func<K, long> hashLambda);
+
+        /// <summary>
+        /// Each hash-index needs a function for checking equality,
+        /// a user can specify the equality-check function via a call
+        /// to this method.
+        /// 
+        /// This method should be used internally by the index grain and
+        /// should not be invoked from other grains.
+        /// </summary>
+        /// <param name="equalsLambda">equality check function that
+        /// should be used for this hash-index</param>
+        void SetEqualsLambda(Func<K, K, bool> equalsLambda);
+
+        /// <summary>
+        /// This method retrieves the result of a lookup into the hash-index
+        /// </summary>
+        /// <param name="key">the lookup key</param>
+        /// <returns>the result of lookup into the hash-index</returns>
+        Task<IEnumerable<V>> Lookup(Immutable<K> key);
+    }
+}
---
+++ src/OrleansIndexing/IRangeIndex.cs
@@ -0,0 +1,54 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrleansIndexing
+{
+    /// <summary>
+    /// Defines the interface for range indexes
+    /// </summary>
+    /// <typeparam name="K">the type of indexed attribute for
+    /// the range index</typeparam>
+    /// <typeparam name="V">the type of grain interface that is
+    /// being indexed</typeparam>
+    interface IRangeIndex<K,V> : IIndex where V : IGrain
+    {
+        /// <summary>
+        /// Given the bounds, this method retrieves the result of
+        /// a lookup into the range index.
+        /// </summary>
+        /// <param name="from">the lower bound of the range</param>
+        /// <param name="to">the upper bound of the range</param>
+        /// <returns>the result of lookup</returns>
+        Task<IEnumerable<V>> LookupRange(K from, K to);
+
+        /// <summary>
+        /// Given the lower bound, this method retrieves the result of
+        /// a lookup into the range index.
+        /// </summary>
+        /// <param name="from">the lower bound of the range</param>
+        /// <returns>the result of lookup</returns>
+        Task<IEnumerable<V>> LookupFromRange(K from);
+
+        /// <summary>
+        /// Given the upper bound, this method retrieves the result of
+        /// a lookup into the range index.
+        /// </summary>
+        /// <param name="to">the upper bound of the range</param>
+        /// <returns>the result of lookup</returns>
+        Task<IEnumerable<V>> LookupToRange(K to);
+
+        /// <summary>
+        /// Each range index requires a comparison function and user
+        /// can provide this comparison function via a call to this method.
+        /// 
+        /// This method should be used internally by the index grain and
+        /// should not be invoked from other grains.
+        /// </summary>
+        /// <param name="compLambda">the comparison function</param>
+        void SetComparisonLambda(Func<K, long> compLambda);
+    }
+}
