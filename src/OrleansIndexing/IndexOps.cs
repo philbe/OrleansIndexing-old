3d6b43292c299ccd3c7538398d184559a739f8f2
Mohammad Dashti <mdashti@gmail.com>
2016-06-08 20:09:46 -0700

    Added the abstract implementation of IIndexOps

---
+++ src/OrleansIndexing/IndexOps.cs
@@ -0,0 +1,20 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrleansIndexing
+{
+    public abstract class IndexOps<T>
+    {
+        abstract public object ExtractIndexImage(T g);
+
+        public IMemberUpdate CreateMemberUpdate(T g, object befImg)
+        {
+            var aftImg = ExtractIndexImage(g);
+            return new MemberUpdate(aftImg, befImg);
+        }
+    }
+}
