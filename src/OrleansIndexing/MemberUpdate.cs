8c1a65e341fffd9ac99010ba3eea93e1ff62956d
Mohammad Dashti <mdashti@gmail.com>
2016-06-08 20:09:04 -0700

    Added MemberUpdate implementation.

---
+++ src/OrleansIndexing/MemberUpdate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrleansIndexing
+{
+    /// <summary>
+    /// MemberUpdate is an generic
+    /// </summary>
+    public class MemberUpdate : IMemberUpdate
+    {
+        private object _befImg;
+        private object _aftImg;
+        private bool _isUpdated;
+
+        public MemberUpdate(object befImg, object aftImg, bool isUpdated)
+        {
+            _isUpdated = isUpdated;
+            if (_isUpdated)
+            {
+                _befImg = befImg;
+                _aftImg = aftImg;
+            }
+        }
+
+        public MemberUpdate(object befImg, object aftImg) : this(befImg, aftImg, befImg.Equals(aftImg))
+        {
+        }
+
+        public object GetBefImg()
+        {
+            return _isUpdated ? _befImg : null;
+        }
+
+        public object GetAftImg()
+        {
+            return _isUpdated ? _aftImg : null;
+        }
+
+        public bool IsUpdated()
+        {
+            return _isUpdated;
+        }
+    }
+}
