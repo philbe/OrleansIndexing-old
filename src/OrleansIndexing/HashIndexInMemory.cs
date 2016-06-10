910de0d9e5bad085b8ad2950f874ca3453449767
Mohammad Dashti <mdashti@gmail.com>
2016-06-09 19:26:16 -0700

    Added the initial implementation of HashIndexInMemory

---
+++ src/OrleansIndexing/HashIndexInMemory.cs
@@ -0,0 +1,113 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans.Concurrency;
+
+namespace Orleans.Indexing
+{
+    /// <summary>
+    /// A simple implementation of a single-grain in-memory hash-index
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public class HashIndexInMemory<K, V> : Grain<HashIndexInMemoryState<K,V>>, IHashIndex<K, V> where V : IGrain
+    {
+        //private Func<K, K, bool> _equalsLambda = ((k1,k2) => k1.Equals(k2));
+        //private Func<K, long> _hashLambda = (k => k.GetHashCode());
+
+        public Task<bool> ApplyIndexUpdate(IGrain g, Immutable<IMemberUpdate> iUpdate)
+        {
+            var updatedGrain = (V)g;
+            var updt = (MemberUpdate)iUpdate.Value;
+            var opType = updt.GetOperationType();
+            if (opType == OperationType.Update)
+            {
+                K befImg = (K)updt.GetBeforeImage();
+
+                if (State.IndexMap.ContainsKey(befImg))
+                {
+                    HashIndexEntry<V> befEntry = State.IndexMap[befImg];
+                    if(befEntry.Values.Contains(updatedGrain))
+                    {
+                        K aftImg = (K)updt.GetAfterImage();
+                        if(State.IndexMap.ContainsKey(aftImg))
+                        {
+                            HashIndexEntry<V> aftEntry = State.IndexMap[aftImg];
+                            if(State.IsUnique && aftEntry.Values.Count > 0)
+                            {
+                                throw new Exception(string.Format("The uniqueness property of index is violated for before-image = {0}, after-image = {1} and grain = {2}", befImg, aftImg, updatedGrain.GetPrimaryKey()));
+                            }
+                            befEntry.Values.Remove(updatedGrain);
+                            aftEntry.Values.Add(updatedGrain);
+                        }
+                        else
+                        {
+                            HashIndexEntry<V> aftEntry = new HashIndexEntry<V>();
+                            aftEntry.Values.Add(updatedGrain);
+                            State.IndexMap.Add(aftImg, aftEntry);
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception(string.Format("The index entry does not exist for before-image = {0} and grain = {1}", befImg, updatedGrain.GetPrimaryKey()));
+                    }
+                }
+                else
+                {
+                    throw new Exception(string.Format("The index entry does not exist for before-image = {0} and grain = {1}", befImg, updatedGrain.GetPrimaryKey()));
+                }
+            }
+            return Task.FromResult(true);
+        }
+
+        public Task<IIndexOps> GetIndexOps()
+        {
+            return Task.FromResult(State.IndexOps);
+        }
+
+        public Task<bool> IsUnique()
+        {
+            return Task.FromResult(State.IsUnique);
+        }
+
+        public Task<IEnumerable<V>> Lookup(Immutable<K> key)
+        {
+            return Task.FromResult((IEnumerable<V>) State.IndexMap[key.Value].Values);
+        }
+        /// <summary>
+        /// Each hash-index needs a hash function, and a user can specify
+        /// the hash function via a call to this method.
+        /// 
+        /// This method should be used internally by the index grain and
+        /// should not be invoked from other grains.
+        /// </summary>
+        /// <param name="hashLambda">hash function that should be used
+        /// for this hash-index</param>
+        //void SetHashLambda(Func<K, long> hashLambda)
+        //{
+        //    _hashLambda = hashLambda;
+        //}
+
+        /// <summary>
+        /// Each hash-index needs a function for checking equality,
+        /// a user can specify the equality-check function via a call
+        /// to this method.
+        /// 
+        /// This method should be used internally by the index grain and
+        /// should not be invoked from other grains.
+        /// </summary>
+        /// <param name="equalsLambda">equality check function that
+        /// should be used for this hash-index</param>
+        //void SetEqualsLambda(Func<K, K, bool> equalsLambda)
+        //{
+        //    _equalsLambda = equalsLambda;
+        //}
+    }
+    public sealed class HashIndexEntry<T>
+    {
+        public ISet<T> Values = new HashSet<T>();
+    }
+}
---
+++ src/OrleansIndexing/HashIndexInMemoryState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Orleans.Indexing
+{
+    /// <summary>
+    /// Contains the state that should be stored for each HashIndexInMemory
+    /// </summary>
+    /// <typeparam name="K">type of hash-index key</typeparam>
+    /// <typeparam name="V">type of grain that is being indexed</typeparam>
+    public class HashIndexInMemoryState<K, V> where V : IGrain
+    {
+        public bool IsUnique { set; get; }
+
+        public IDictionary<K, HashIndexEntry<V>> IndexMap { set; get; }
+
+        public IIndexOps IndexOps { set; get; }
+    }
+}
