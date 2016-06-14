3bc5d70b911de239bdded58ea8f99fddf1d857da
Mohammad Dashti <mdashti@gmail.com>
2016-06-13 17:38:15 -0700

    Added TypeExtensions in order to extract the generic types with their parameters

---
+++ src/OrleansIndexing/TypeExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.Indexing
+{
+    public static class TypeExtensions
+    {
+        public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
+        {
+            return givenType.GetInterfaces().Any(t => t.IsGenericType && t.GetGenericTypeDefinition() == genericType) ||
+                   givenType.BaseType != null && (givenType.BaseType.IsGenericType && givenType.BaseType.GetGenericTypeDefinition() == genericType ||
+                                                  givenType.BaseType.IsAssignableToGenericType(genericType));
+        }
+
+        public static Type GetGenericType(this Type givenType, Type genericType)
+        {
+            var interfaceTypes = givenType.GetInterfaces();
+
+            foreach (var it in interfaceTypes)
+            {
+                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
+                    return it;
+            }
+
+            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+                return givenType;
+
+            Type baseType = givenType.BaseType;
+            if (baseType == null) return null;
+
+            return GetGenericType(baseType, genericType);
+        }
+    }
+}
