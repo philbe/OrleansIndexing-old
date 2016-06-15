ecde49a7299e612ba7bf41cb20e0e3fc037cd717
Mohammad Dashti <mdashti@gmail.com>
2016-06-14 20:12:14 -0700

    Added a new method to GrainFactoryExtensions

--- src/OrleansIndexing/GrainFactoryExtension.cs
+++ src/OrleansIndexing/GrainFactoryExtension.cs
@@ -10,11 +10,18 @@ namespace Orleans.Indexing
 {
     public static class GrainExtensions
     {
-        public static IGrainType GetGrain<IGrainType>(this IGrainFactory gf, string grainID, Type grainInterfaceType)
+        public static OutputGrainInterfaceType GetGrain<OutputGrainInterfaceType>(this IGrainFactory gf, string grainID, Type grainInterfaceType) where OutputGrainInterfaceType : IGrain
         {
-            return GrainFactoryBase.MakeGrainReference_FromType(
+            //return GrainFactoryBase.MakeGrainReference_FromType(
+            //        baseTypeCode => TypeCodeMapper.ComposeGrainId(baseTypeCode, grainID, grainInterfaceType),
+            //        grainInterfaceType).AsReference<OutputGrainInterfaceType>();
+            return GetGrain<OutputGrainInterfaceType>(gf, grainID, grainInterfaceType, typeof(OutputGrainInterfaceType));
+        }
+        public static SuperOutputGrainInterfaceType GetGrain<SuperOutputGrainInterfaceType>(this IGrainFactory gf, string grainID, Type grainInterfaceType, Type outputGrainInterfaceType)
+        {
+            return (SuperOutputGrainInterfaceType)((GrainFactory)gf).Cast(GrainFactoryBase.MakeGrainReference_FromType(
                     baseTypeCode => TypeCodeMapper.ComposeGrainId(baseTypeCode, grainID, grainInterfaceType),
-                    grainInterfaceType).AsReference<IGrainType>();
+                    grainInterfaceType), outputGrainInterfaceType);
         }
     }
 }
