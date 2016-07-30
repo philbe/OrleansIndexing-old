1df5cd1afba67b9d2893fdb5229d3e94f57af49e
Mohammad Dashti <mdashti@gmail.com>
2016-07-29 20:01:14 -0700

    Added methods to IndexableGrain for finding the designated IndexWorkflowQueue and ReincarnatedIndexWorkflowQueue

--- src/OrleansIndexing/Core/IndexableGrain.cs
+++ src/OrleansIndexing/Core/IndexableGrain.cs
@@ -17,6 +17,19 @@ namespace Orleans.Indexing
     ///     1- the grain class should extend IndexableGrain
     ///     2- the grain class is responsible for calling UpdateIndexes
     ///        whenever one or more indexes need to be updated
+    ///        
+    /// Fault tolerance can be an optional feature for indexing, i.e.,
+    /// IndexableGrain extends IndexableGrainNonFaultTolerant.
+    /// By default, indexing is fault tolerant.
+    /// 
+    /// IndexableGrain creates a wrapper around the State class provided by
+    /// the actual user-grain that extends it. It adds the following information to it:
+    ///  - a list called ActiveWorkflowsList to the State,
+    ///    which points to the in-flight indexing workflowsIds.
+    ///  - There's a fixed mapping (e.g., a hash function) from grain id to IndexWorkflowQueue
+    ///    instance. Each IndexableGrain G has a property workflowQueue whose value,
+    ///    [grain-type-name + sequence number], identifies the IndexWorkflowQueue grain
+    ///    that processes index updates on G's behalf.
     /// </summary>
     public abstract class IndexableGrain<TState, TProperties> : IndexableGrainNonFaultTolerant<IndexableExtendedState<TState>, TProperties> where TProperties: new()
     {
@@ -34,12 +47,51 @@ namespace Orleans.Indexing
             }
             return _props;
         }
+
+        private string GetWorkflowQueue()
+        {
+            return TypeUtils.GetFullName(typeof(TState)) + "-" + GetWorkflowQueueId();
+        }
+
+        private int GetWorkflowQueueId()
+        {
+            //does it have a designated IndexWorkflowQueue?
+            if (State.WorkflowQueueSeqNum > 0)
+            {
+                return State.WorkflowQueueSeqNum;
+            }
+            else
+            {
+                //TODO: find the designated IndexWorkflowQueue sequence number on this silo
+                return 1;
+            }
+        }
+
+        private SiloAddress GetWorkflowQueueSilo()
+        {
+            //does it have a designated IndexWorkflowQueue?
+            if (State.WorkflowQueueSeqNum > 0)
+            {
+                return State.WorkflowQueueSilo;
+            }
+            else
+            {
+                return RuntimeAddress;
+            }
+        }
+
+        private string GetReincarnatedWorkflowQueueId()
+        {
+            return GetWorkflowQueue() + "/" + GetWorkflowQueueSilo().ToLongString();
+        }
     }
 
     /// <summary>
-    /// 
+    /// IndexableExtendedState{TState} is a wrapper around
+    /// a user-defined state, TState, which adds the necessary
+    /// information for fault-tolerant indexing
     /// </summary>
-    /// <typeparam name="TState"></typeparam>
+    /// <typeparam name="TState">the type of user state</typeparam>
     [Serializable]
     public class IndexableExtendedState<TState>
     {
