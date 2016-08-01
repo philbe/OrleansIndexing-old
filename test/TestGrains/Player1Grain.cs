227fecd4701e9800f2b878dd434e0d460ab07d29
Mohammad Dashti <mdashti@gmail.com>
2016-08-01 10:27:29 -0700

    Refactored PlayerGrain classes to avoid having several copies of the same code

--- test/TestGrains/Player1Grain.cs
+++ test/TestGrains/Player1Grain.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Threading.Tasks;
-using Orleans;
 using Orleans.Providers;
-using Orleans.Runtime;
 using UnitTests.GrainInterfaces;
-using Orleans.Indexing;
 
 namespace UnitTests.Grains
 {
     [Serializable]
-    public class Player1GrainState : Player1Properties
+    public class Player1GrainState : Player1Properties, PlayerGrainState
     {
         public string Email { get; set; }
     }
@@ -18,60 +14,7 @@ namespace UnitTests.Grains
     /// A simple grain that represent a player in a game
     /// </summary>
     [StorageProvider(ProviderName = "MemoryStore")]
-    public class Player1Grain : IndexableGrainNonFaultTolerant<Player1GrainState, Player1Properties>, IPlayer1Grain
+    public class Player1Grain : PlayerGrainNonFaultTolerant<Player1GrainState, Player1Properties>, IPlayer1Grain
     {
-        private Logger logger;
-        
-        public string Email { get { return State.Email; } }
-        public string Location { get { return State.Location; } }
-        public int Score { get { return State.Score; } }
-
-        public override Task OnActivateAsync()
-        {
-            logger = GetLogger("PlayerGrain-" + IdentityString);
-            return base.OnActivateAsync();
-        }
-
-        public Task<string> GetLocation()
-        {
-            return Task.FromResult(Location);
-        }
-
-        public Task SetLocation(string location)
-        {
-            State.Location = location;
-            //return TaskDone.Done;
-            return base.WriteStateAsync();
-        }
-
-        public Task<int> GetScore()
-        {
-            return Task.FromResult(Score);
-        }
-
-        public Task SetScore(int score)
-        {
-            State.Score = score;
-            //return TaskDone.Done;
-            return base.WriteStateAsync();
-        }
-
-        public Task<string> GetEmail()
-        {
-            return Task.FromResult(Email);
-        }
-
-        public Task SetEmail(string email)
-        {
-            State.Email = email;
-            //return TaskDone.Done;
-            return base.WriteStateAsync();
-        }
-
-        public Task Deactivate()
-        {
-            DeactivateOnIdle();
-            return TaskDone.Done;
-        }
     }
 }
---
+++ test/TestGrains/PlayerGrainState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Providers;
+using Orleans.Runtime;
+using UnitTests.GrainInterfaces;
+using Orleans.Indexing;
+
+namespace UnitTests.Grains
+{
+    public interface PlayerGrainState
+    {
+        int Score { get; set; }
+        string Location { get; set; }
+        string Email { get; set; }
+    }
+}
